bf64aa2dec63de3bb4b5878d79070f32e148bb8b
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2024-12-06 19:34:48 +0200

    initial api :)

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\UserService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    protected  $userService = null;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function index()
+    {
+        $users = $this->userService->getAll();
+        return response()->json($users);
+    }
+}
